29e3d322663d56fee53b17b492c80a6f8a65a8f6
Sam Barrick <you@example.com>
2020-01-19 22:13:51 -0600

    ready for turn in! Fun assignment

--- src/Toggle.js
+++ src/Toggle.js
@@ -1,30 +1,35 @@
 import React, { Component } from 'react'
 
-//Kev and I worked on this the toggle button together. Found a really helpful video for this at:
-//https://www.youtube.com/watch?time_continue=311&v=x5oiX93DeHA&feature=emb_logo
-//Great resource.
+//Kev and I worked on the toggle button together. Found a really helpful video for this at:
+//https://www.youtube.com/watch?time_continue=311&v=x5oiX93DeHA&feature=emb_logo and
+//https://www.youtube.com/watch?time_continue=6&v=2Zz0K1U4Dk8&feature=emb_logo
+//Great resource. Found the video when working together but worked on it separately so variable names may be different...
+//Since this was a group project, we tag teamed it and rephrased things in a way we each understood on our own.
 
 export default class Toggle extends Component {
    state = {
-       on: false,
-       text: "Show"
+       on: false //set to false since nothing has been done yet.
    }
 
    toggle = () => {
-       this.setState({
-           on: !this.state.on,
-       })
+    const newState = !this.state.on
+        this.setState({on:newState})
    }
 
-   changeText = (text) => {
-    this.setState({ text }); 
-   }
-   
+   changeText = () => {
+    const newState = !this.state.switch
+    this.setState({ switch:newState }); 
+  } 
+  
     render() {
+
+        const { on } = this.state;
+        const buttonText = on?"Hide":"Show";
+
         return (
             <div>
             {this.state.on && this.props.children}
-                <button onClick={this.toggle}>Show/Hide</button>
+                <button onClick= {this.toggle}>{buttonText}</button>
             </div> 
         );
     }
